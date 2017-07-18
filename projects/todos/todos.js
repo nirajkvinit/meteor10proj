ac9835e95ba6a9f93f09c40778d060d716dcb79b
Niraj Kumar <nirajkvinit@gmail.com>
2017-07-18 12:00:24 +0530

    Chapter 5 completed

--- projects/todos/todos.js
+++ projects/todos/todos.js
@@ -4,7 +4,28 @@ if (Meteor.isClient) {
   // Template Helpers
   Template.main.helpers({
     todos: function(){
-      return Todos.find();
+      return Todos.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.main.events({
+    "submit .new-todo": function(event){      
+      var text = event.target.text.value;
+
+      Todos.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
+      return false;
+    },
+    "click .toggle-checked": function(){
+      Todos.update(this._id, {$set: {checked: !this.checked}});
+    },
+    "click .delete-todo": function(){
+      if(confirm("Are you sure?")) {
+        Todos.remove(this._id);        
+      }      
     }
   });
 }
