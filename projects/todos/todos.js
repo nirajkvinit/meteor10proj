276c3843404ff04993e132a66a7fa96a5eab5423
Niraj Kumar <nirajkvinit@gmail.com>
2017-07-18 12:27:39 +0530

    Chapter 6 completed

--- projects/todos/todos.js
+++ projects/todos/todos.js
@@ -11,25 +11,51 @@ if (Meteor.isClient) {
   Template.main.events({
     "submit .new-todo": function(event){      
       var text = event.target.text.value;
+      
+      Meteor.call('addTodo', text);
 
-      Todos.insert({
-        text: text,
-        createdAt: new Date()
-      });
       event.target.text.value = "";
       return false;
     },
     "click .toggle-checked": function(){
-      Todos.update(this._id, {$set: {checked: !this.checked}});
+      //Todos.update(this._id, {$set: {checked: !this.checked}});
+      Meteor.call('setChecked', this._id, !this.checked);
     },
     "click .delete-todo": function(){
       if(confirm("Are you sure?")) {
-        Todos.remove(this._id);        
+        //Todos.remove(this._id);        
+        Meteor.call('deleteTodo', this._id);
       }      
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
+
+//Meteor Methods
+Meteor.methods({
+  addTodo: function(text){
+    if(!Meteor.userId()) {
+      throw new Meteor.Error('Not authorized');
+    }
+    Todos.insert({
+        text: text,
+        createdAt: new Date(),
+        userId: Meteor.userId(),
+        username: Meteor.user().username
+      });
+  },
+  deleteTodo: function(todoId){
+    Todos.remove(todoId);
+  },
+  setChecked: function(todoId, setChecked){
+    Todos.update(todoId, {$set: {checked: setChecked}})
+  }
+});
 
 if (Meteor.isServer) {
   
 }
+
